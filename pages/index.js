03c4367244c25c694fd102befda274cb3217a306
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-11 11:35:03 -0500

    Added posts and post access to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 import Layout from '../components/Layout';
 
-export default function HomePage() {
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace('.md', '');
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', fileName),
+      'utf-8'
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+}
+
+export default function HomePage({ posts }) {
+  console.log(posts);
   return (
     <Layout>
       <h1>Hello there.</h1>
