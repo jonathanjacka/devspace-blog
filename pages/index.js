29bdc56a7e62a71dbce1c7cea8140075849703f1
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-11 19:30:32 -0500

    Added Post component for each display of post on homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
 import path from 'path';
+import Link from 'next/link';
 import matter from 'gray-matter';
 import Layout from '../components/Layout';
+import Post from '../components/Post';
 
 export async function getStaticProps() {
   const files = fs.readdirSync(path.join('posts'));
@@ -28,10 +30,21 @@ export async function getStaticProps() {
 }
 
 export default function HomePage({ posts }) {
-  console.log(posts);
   return (
     <Layout>
-      <h1>Hello there.</h1>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Lastest Posts</h1>
+
+      <div className='grid md:grid-cols-2  lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+
+      <Link href='/blog'>
+        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
 }
