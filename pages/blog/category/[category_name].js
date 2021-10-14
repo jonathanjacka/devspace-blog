87e46c11aba27c9d9ccaa54e19347b344ef80671
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-14 16:55:24 -0500

    Added lib/posts.js to clean up fetching data from files

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 import Link from 'next/link';
-import matter from 'gray-matter';
 import Layout from '@/components/Layout';
 import Post from '@/components/Post';
-import { sortByDate } from '@/utils/index';
+import matter from 'gray-matter';
+import { getPosts } from '@/lib/posts';
 
 export async function getStaticPaths() {
   const files = fs.readdirSync(path.join('posts'));
@@ -33,21 +33,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { category_name } }) {
   const files = fs.readdirSync(path.join('posts'));
 
-  const posts = files.map((fileName) => {
-    const slug = fileName.replace('.md', '');
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', fileName),
-      'utf-8'
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  const posts = getPosts();
 
   //Filter posts by category
   const categoryPosts = posts.filter(
@@ -56,7 +42,7 @@ export async function getStaticProps({ params: { category_name } }) {
 
   return {
     props: {
-      posts: categoryPosts.sort(sortByDate),
+      posts: categoryPosts,
       categoryName: category_name,
     },
   };
@@ -66,7 +52,7 @@ export default function CategoryBlogPage({ posts, categoryName }) {
   return (
     <Layout>
       <h1 className='text-5xl border-b-4 p-5 font-bold'>
-        Posts in {categoryName}
+        Posts in {categoryName.charAt(0).toUpperCase() + categoryName.slice(1)}
       </h1>
 
       <div className='grid md:grid-cols-2  lg:grid-cols-3 gap-5'>
@@ -74,12 +60,6 @@ export default function CategoryBlogPage({ posts, categoryName }) {
           <Post key={index} post={post} />
         ))}
       </div>
-
-      <Link href='/blog'>
-        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
-          All Posts
-        </a>
-      </Link>
     </Layout>
   );
 }
