fe3f4baa9a0956b17ebf1b24314dae3272f4e787
Jonathan Jacka <jonathanjacka@gmail.com>
2021-10-15 06:18:58 -0500

    Updated components/Search.js

--- components/Search.js
+++ components/Search.js
@@ -12,7 +12,6 @@ export default function Search() {
       } else {
         const res = await fetch(`/api/search?q=${searchTerm}`);
         const { results } = await res.json();
-        console.log(results);
         setSearchResults(results);
       }
     };
